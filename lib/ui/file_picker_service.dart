6a4f9abaa3693d847adfd90a07edb40f39e5acb9
Phyo Wai Aung <phyowaiaung.ygn.85@gmail.com>
2024-07-23 13:59:29 +0630

    pwa 230724

---
+++ lib/ui/file_picker_service.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FilePickerService {
+  static Map<String, String> header = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json'
+  };
+
+  Future<File?> pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      return File(result.files.single.path!);
+    }
+    return null;
+  }
+
+  Future<void> uploadFile(File file, String url) async {
+    late String serverIP;
+    late SharedPreferences sharedPreferences;
+    sharedPreferences = await SharedPreferences.getInstance();
+    serverIP = (sharedPreferences.getString('serverIP')! != '') ? sharedPreferences.getString('serverIP')! : 'http://103.47.185.240:99';
+    String attachfileUrl = '/api/Attach_File/AttachFile';
+
+    var request = http.MultipartRequest('POST', Uri.parse(attachfileUrl));
+    request.files.add(await http.MultipartFile.fromPath('file', file.path));
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('File uploaded successfully.');
+    } else {
+      print('File upload failed.');
+    }
+  }
+}
